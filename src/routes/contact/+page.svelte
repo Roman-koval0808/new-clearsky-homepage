4696a033602ab19205a4c3ce18b32cebba91aab4
Roman Kovalenko <roman.kovalenko0808@outlook.com>
2026-08-17 15:57:07 +0100

    feat: embed leadform script in contact page contact form container

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -12,11 +12,18 @@
   let leadboxContainer;
 
   onMount(() => {
-    const script = document.createElement('script');
-    script.src = "https://a2p.viewroom.ca/embed/leadbox/cmkwq0cf20001f4gpo4ir2n3a?t=1786976631020";
-    script.async = true;
+    const leadboxScript = document.createElement('script');
+    leadboxScript.src = "https://a2p.viewroom.ca/embed/leadbox/cmkwq0cf20001f4gpo4ir2n3a?t=1786976631020";
+    leadboxScript.async = true;
     if (leadboxContainer) {
-      leadboxContainer.appendChild(script);
+      leadboxContainer.appendChild(leadboxScript);
+    }
+
+    const leadformScript = document.createElement('script');
+    leadformScript.src = "https://a2p.viewroom.ca/embed/leadform/cmqrr1w61000912m8vq2b9z2u?t=1786978361272";
+    leadformScript.async = true;
+    if (leadboxContainer) {
+      leadboxContainer.appendChild(leadformScript);
     }
   });
 </script>
